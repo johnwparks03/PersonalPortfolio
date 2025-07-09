436124662206f3c4f43d92f869c0cecbbabde902
John Parks <johnwparks03@gmail.com>
2025-07-09 19:16:28 -0400

    add effect to zoom in on timeline item in middle of the screen

--- PersonalPortfolio/src/app/work-experience-component/work-experience.component.ts
+++ PersonalPortfolio/src/app/work-experience-component/work-experience.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  Renderer2,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
-  styleUrls: ['./work-experience.component.css']
+  styleUrls: ['./work-experience.component.css'],
 })
-export class WorkExperienceComponent {
+export class WorkExperienceComponent implements AfterViewInit{
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    this.onScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const items = this.el.nativeElement.querySelectorAll('.timeline-item');
+    let minDiff = Infinity;
+    let activeItem: HTMLElement | null = null;
+    const centerOfScreen = window.innerHeight / 2;
+
+    items.forEach((item: HTMLElement) => {
+      const rect = item.getBoundingClientRect();
+      const itemCenter = rect.top + rect.height / 2;
+      const diff = Math.abs(itemCenter - centerOfScreen);
+      if (diff < minDiff) {
+        minDiff = diff;
+        activeItem = item;
+      }
+      this.renderer.removeClass(item, 'active');
+    });
+
+    if (activeItem) {
+      this.renderer.addClass(activeItem, 'active');
+      console.log(activeItem);
+    }
+  }
 }
