b17e660af212f238ac444206a25052b89651307a
John Parks <johnwparks03@gmail.com>
2025-07-29 00:43:34 -0400

    Fix bug with pdfs not loading from assets

--- PersonalPortfolio/src/app/pdf-viewer-dialog/pdf-viewer-dialog.component.ts
+++ PersonalPortfolio/src/app/pdf-viewer-dialog/pdf-viewer-dialog.component.ts
@@ -1,16 +1,24 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-pdf-viewer-dialog',
   templateUrl: './pdf-viewer-dialog.component.html',
-  styleUrls: ['./pdf-viewer-dialog.component.css']
+  styleUrls: ['./pdf-viewer-dialog.component.css'],
 })
 export class PdfViewerDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<PdfViewerDialogComponent>,
+    @Inject(DOCUMENT) private document: Document,
     @Inject(MAT_DIALOG_DATA) public data: { url: string }
-  ) {}
+  ) {
+    const test = this.document.getElementsByTagName('base')[0].href;
+    const segments = test.split('/');
+    const lastSegment = segments[segments.length - 1];
+
+    data.url = lastSegment ? lastSegment + '/' + data.url : data.url;
+  }
 
   close(): void {
     this.dialogRef.close();
