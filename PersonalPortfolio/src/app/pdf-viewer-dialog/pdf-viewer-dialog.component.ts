ed4fc738b3109886a50266821cc6b6611220d8cf
John Parks <johnwparks03@gmail.com>
2025-07-29 21:10:04 -0400

    Implemented fix for pdf viewer

--- PersonalPortfolio/src/app/pdf-viewer-dialog/pdf-viewer-dialog.component.ts
+++ PersonalPortfolio/src/app/pdf-viewer-dialog/pdf-viewer-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-pdf-viewer-dialog',
@@ -10,19 +9,8 @@ import { DOCUMENT } from '@angular/common';
 export class PdfViewerDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<PdfViewerDialogComponent>,
-    @Inject(DOCUMENT) private document: Document,
     @Inject(MAT_DIALOG_DATA) public data: { url: string }
-  ) {
-    const test = this.document.getElementsByTagName('base')[0].href;
-    const segments = test.split('/').filter(Boolean); // removes empty strings
-    const lastSegment = segments[segments.length - 1];
-    console.log('Last segment:', lastSegment);
-    console.log('Segments:', segments);
-    console.log('Base HREF:', test);
-    console.log('Data URL:', data.url);
-
-    // data.url = lastSegment ? lastSegment + '/' + data.url : data.url;
-  }
+  ) {}
 
   close(): void {
     this.dialogRef.close();
